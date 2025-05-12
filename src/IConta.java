38bf3436709fb16d1ef295fb991feea5c8297d53
essinformatica <essinformatica15@gmail.com>
2025-05-12 14:18:15 -0300

    desafio bancodigital

--- src/IConta.java
+++ src/IConta.java
@@ -1,11 +1,15 @@
 
 public interface IConta {
-	
-	void sacar(double valor);
-	
-	void depositar(double valor);
-	
-	void transferir(double valor, IConta contaDestino);
-	
-	void imprimirExtrato();
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, IConta contaDestino);
+
+    void imprimirExtrato();
+
+    void enviarPix(Conta numeroPix, double valor);
+
+    void receberPix(double valor);
 }
